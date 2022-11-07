b5a9e8922ab978ed621440b6c2859f3149fa34fe
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-07 08:51:41 -0600

    dictionary for vb keyboard input

--- src/DungeonDelverVB/Program.vb
+++ src/DungeonDelverVB/Program.vb
@@ -2,6 +2,17 @@ Imports System.Runtime.InteropServices
 Module Program
     Const SCREEN_WIDTH = 1280
     Const SCREEN_HEIGHT = 720
+    Const COMMAND_ZERO = "0"c
+    Const COMMAND_ONE = "1"c
+    Const COMMAND_TWO = "2"c
+    Const COMMAND_THREE = "3"c
+    Const COMMAND_FOUR = "4"c
+    Const COMMAND_FIVE = "5"c
+    Const COMMAND_SIX = "6"c
+    Const COMMAND_SEVEN = "7"c
+    Const COMMAND_EIGHT = "8"c
+    Const COMMAND_NINE = "9"c
+    Const COMMAND_DOT = "."c
     Sub Main(args As String())
         If SDL_Init(&H10F231) = 0 Then
             Dim window As IntPtr = IntPtr.Zero
@@ -34,36 +45,51 @@ Module Program
             SDL_Quit()
         End If
     End Sub
+    ReadOnly keyTable As IReadOnlyDictionary(Of UInt32, Char) =
+        New Dictionary(Of UInt32, Char) From
+        {
+             {SDLK_PERIOD, "."c},
+             {SDLK_0, "0"c},
+             {SDLK_1, "1"c},
+             {SDLK_2, "2"c},
+             {SDLK_3, "3"c},
+             {SDLK_4, "4"c},
+             {SDLK_5, "5"c},
+             {SDLK_6, "6"c},
+             {SDLK_7, "7"c},
+             {SDLK_8, "8"c},
+             {SDLK_9, "9"c},
+             {SDLK_DELETE, "."c},
+             {SDLK_INSERT, "0"c},
+             {SDLK_HOME, "7"c},
+             {SDLK_PAGEUP, "9"c},
+             {SDLK_END, "1"c},
+             {SDLK_PAGEDOWN, "3"c},
+             {SDLK_RIGHT, "6"c},
+             {SDLK_LEFT, "4"c},
+             {SDLK_DOWN, "2"c},
+             {SDLK_UP, "8"c},
+             {SDLK_KP_1, "1"c},
+             {SDLK_KP_2, "2"c},
+             {SDLK_KP_3, "3"c},
+             {SDLK_KP_4, "4"c},
+             {SDLK_KP_5, "5"c},
+             {SDLK_KP_6, "6"c},
+             {SDLK_KP_7, "7"c},
+             {SDLK_KP_8, "8"c},
+             {SDLK_KP_9, "9"c},
+             {SDLK_KP_0, "0"c},
+             {SDLK_KP_PERIOD, "."c}
+        }
     Private Function HandleKeyDown(sym As UInteger) As Boolean
-        Select Case sym
-            Case 46, 1073741923, 127
-                Return HandleCommand("."c)
-            Case 48, 1073741922, 1073741897
-                Return HandleCommand("0"c)
-            Case 49, 1073741913, 1073741901
-                Return HandleCommand("1"c)
-            Case 50, 1073741914, 1073741905
-                Return HandleCommand("2"c)
-            Case 51, 1073741915, 1073741902
-                Return HandleCommand("3"c)
-            Case 52, 1073741916, 1073741904
-                Return HandleCommand("4"c)
-            Case 53, 1073741917
-                Return HandleCommand("5"c)
-            Case 54, 1073741918, 1073741903
-                Return HandleCommand("6"c)
-            Case 55, 1073741919, 1073741898
-                Return HandleCommand("7"c)
-            Case 56, 1073741920, 1073741906
-                Return HandleCommand("8"c)
-            Case 57, 1073741921, 1073741899
-                Return HandleCommand("9"c)
-            Case Else
-                Return True
-        End Select
+        Dim character As Char = Char.MinValue
+        If keyTable.TryGetValue(sym, character) Then
+            Return HandleCommand(character)
+        End If
+        Return True
     End Function
 
     Private Function HandleCommand(command As Char) As Boolean
-        Return command <> "."c
+        Return True
     End Function
 End Module
--- src/DungeonDelverVB/SDL2.vb
+++ src/DungeonDelverVB/SDL2.vb
@@ -22,4 +22,36 @@ Module SDL2
                                                   ByRef [event] As SDL_Event) As Integer
     Declare Sub SDL_DestroyRenderer Lib "SDL2.dll" (ByVal renderer As IntPtr)
     Declare Sub SDL_DestroyWindow Lib "SDL2.dll" (ByVal window As IntPtr)
+    Friend Const SDLK_PERIOD As UInt32 = 46
+    Friend Const SDLK_0 As UInt32 = 48
+    Friend Const SDLK_1 As UInt32 = 49
+    Friend Const SDLK_2 As UInt32 = 50
+    Friend Const SDLK_3 As UInt32 = 51
+    Friend Const SDLK_4 As UInt32 = 52
+    Friend Const SDLK_5 As UInt32 = 53
+    Friend Const SDLK_6 As UInt32 = 54
+    Friend Const SDLK_7 As UInt32 = 55
+    Friend Const SDLK_8 As UInt32 = 56
+    Friend Const SDLK_9 As UInt32 = 57
+    Friend Const SDLK_DELETE As UInt32 = 127
+    Friend Const SDLK_INSERT = 1073741897
+    Friend Const SDLK_HOME As UInt32 = 1073741898
+    Friend Const SDLK_PAGEUP As UInt32 = 1073741899
+    Friend Const SDLK_END As UInt32 = 1073741901
+    Friend Const SDLK_PAGEDOWN As UInt32 = 1073741902
+    Friend Const SDLK_RIGHT As UInt32 = 1073741903
+    Friend Const SDLK_LEFT As UInt32 = 1073741904
+    Friend Const SDLK_DOWN As UInt32 = 1073741905
+    Friend Const SDLK_UP As UInt32 = 1073741906
+    Friend Const SDLK_KP_1 As UInt32 = 1073741913
+    Friend Const SDLK_KP_2 As UInt32 = 1073741914
+    Friend Const SDLK_KP_3 As UInt32 = 1073741915
+    Friend Const SDLK_KP_4 As UInt32 = 1073741916
+    Friend Const SDLK_KP_5 As UInt32 = 1073741917
+    Friend Const SDLK_KP_6 As UInt32 = 1073741918
+    Friend Const SDLK_KP_7 As UInt32 = 1073741919
+    Friend Const SDLK_KP_8 As UInt32 = 1073741920
+    Friend Const SDLK_KP_9 As UInt32 = 1073741921
+    Friend Const SDLK_KP_0 As UInt32 = 1073741922
+    Friend Const SDLK_KP_PERIOD As UInt32 = 1073741923
 End Module
