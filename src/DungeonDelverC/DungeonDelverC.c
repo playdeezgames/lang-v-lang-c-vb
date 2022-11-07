f2ec9fe6caa03e2ec6f885ddd43cd320e912beae
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-07 05:39:59 -0600

    c now handles keyboard input

--- src/DungeonDelverC/DungeonDelverC.c
+++ src/DungeonDelverC/DungeonDelverC.c
@@ -4,28 +4,99 @@ enum
 	SCREEN_WIDTH = 1280,
 	SCREEN_HEIGHT = 720
 };
+int HandleCommand(char command)
+{
+	return 1;
+}
+int HandleKeyDown(SDL_KeyCode code)
+{
+	switch (code)
+	{
+	case SDLK_INSERT:
+	case SDLK_KP_0:
+	case SDLK_0:
+		HandleCommand('0');
+		break;
+	case SDLK_END:
+	case SDLK_KP_1:
+	case SDLK_1:
+		HandleCommand('1');
+		break;
+	case SDLK_DOWN:
+	case SDLK_KP_2:
+	case SDLK_2:
+		HandleCommand('2');
+		break;
+	case SDLK_KP_3:
+	case SDLK_3:
+	case SDLK_PAGEDOWN:
+		HandleCommand('3');
+		break;
+	case SDLK_KP_4:
+	case SDLK_LEFT:
+	case SDLK_4:
+		HandleCommand('4');
+		break;
+	case SDLK_KP_5:
+	case SDLK_5:
+		HandleCommand('5');
+		break;
+	case SDLK_KP_6:
+	case SDLK_6:
+	case SDLK_RIGHT:
+		HandleCommand('6');
+		break;
+	case SDLK_HOME:
+	case SDLK_KP_7:
+	case SDLK_7:
+		HandleCommand('7');
+		break;
+	case SDLK_UP:
+	case SDLK_KP_8:
+	case SDLK_8:
+		HandleCommand('8');
+		break;
+	case SDLK_KP_9:
+	case SDLK_PAGEUP:
+	case SDLK_9:
+		HandleCommand('9');
+		break;
+	case SDLK_KP_PERIOD:
+	case SDLK_PERIOD:
+	case SDLK_DELETE:
+		HandleCommand('.');
+		break;
+	default:
+		return 1;
+	}
+}
 int main(int argc, char** argv)
 {
-	SDL_Window* window = 0;
-	SDL_Renderer* renderer = 0;
 	if (0 == SDL_Init(SDL_INIT_EVERYTHING))
 	{
+		SDL_Window* window = 0;
+		SDL_Renderer* renderer = 0;
 		if (0 == SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer))
 		{
 			//initialize application
 			SDL_Event event = { 0 };
-			for (;;)
+			int running = 1;
+			for (;running;)
 			{
 				SDL_RenderClear(renderer);
 				//draw to renderer
 				SDL_RenderPresent(renderer);
 				if (1 == SDL_WaitEvent(&event))
 				{
-					if (SDL_QUIT == event.type)
+					switch (event.type)
 					{
+					case SDL_QUIT:
+						running = 0;
+						break;
+					case SDL_KEYDOWN:
+						running = HandleKeyDown(event.key.keysym.sym);
 						break;
 					}
-					//handle event
 				}
 			}
 			//clean up application
--- src/DungeonDelverVB/Program.vb
+++ src/DungeonDelverVB/Program.vb
@@ -2,25 +2,6 @@ Imports System.Runtime.InteropServices
 Module Program
     Const SCREEN_WIDTH = 1280
     Const SCREEN_HEIGHT = 720
-    Declare Function SDL_Init Lib "SDL2.dll" (flags As UInt32) As Integer
-    Declare Function SDL_CreateWindowAndRenderer Lib "SDL2.dll" (
-                                                                ByVal width As Integer,
-                                                                ByVal height As Integer,
-                                                                ByVal window_flags As UInt32,
-                                                                ByRef window As IntPtr,
-                                                                ByRef renderer As IntPtr) As Integer
-    Declare Function SDL_RenderClear Lib "SDL2.dll" (ByVal renderer As IntPtr) As Integer
-    Declare Function SDL_RenderPresent Lib "SDL2.dll" (ByVal renderer As IntPtr) As Integer
-    Declare Sub SDL_Quit Lib "SDL2.dll" ()
-    <StructLayout(LayoutKind.Explicit, Size:=56)>
-    Structure SDL_Event
-        <FieldOffset(0)>
-        Public type As UInt32
-    End Structure
-    Declare Function SDL_WaitEvent Lib "SDL2.dll" (
-                                                  ByRef [event] As SDL_Event) As Integer
-    Declare Sub SDL_DestroyRenderer Lib "SDL2.dll" (ByVal renderer As IntPtr)
-    Declare Sub SDL_DestroyWindow Lib "SDL2.dll" (ByVal window As IntPtr)
     Sub Main(args As String())
         If SDL_Init(&H10F231) = 0 Then
             Dim window As IntPtr = IntPtr.Zero
