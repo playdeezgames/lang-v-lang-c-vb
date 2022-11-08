141b564946f639fff972ac28d05fc78accc55c04
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-08 05:49:45 -0600

    the dude moves around the room

--- src/DungeonDelverC/Main.c
+++ src/DungeonDelverC/Main.c
@@ -10,18 +10,23 @@ enum {
 	LOGICAL_WIDTH = SCREEN_WIDTH / 2,
 	LOGICAL_HEIGHT = SCREEN_HEIGHT / 2,
 	GRID_COLUMNS = LOGICAL_WIDTH/ CELL_WIDTH,
-	GRID_ROWS = LOGICAL_HEIGHT/CELL_HEIGHT
+	GRID_ROWS = LOGICAL_HEIGHT/CELL_HEIGHT,
+	GRID_COUNT = GRID_COLUMNS * GRID_ROWS
 };
 extern SDL_Rect srcRects[];
 extern SDL_Rect dstRects[];
+extern unsigned char map[];
 int main(int argc, char** argv)
 {
 	SDL_Window* window = 0;
 	SDL_Renderer* renderer = 0;
 	SDL_Event event = { 0 };
 	SDL_Texture* texture = 0;
-	int index = 0;
+	unsigned char old;
+	int index = GRID_COLUMNS/2 + (GRID_ROWS/2) * GRID_COLUMNS;
+	int nextIndex = 0;
 	int delta = 0;
+	int cell = 0;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING)) goto PreinitializationFailure;
 	IMG_Init(IMG_INIT_PNG);
@@ -30,9 +35,18 @@ int main(int argc, char** argv)
 	texture = IMG_LoadTexture(renderer, "romfont8x8.png");
 
 StartDraw:
+	old = map[index];
+	map[index] = 2;
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, srcRects+2, dstRects+index);
+	cell = 0;
+StartRenderCell:
+	if (GRID_COUNT == cell) goto EndRenderCell;
+	SDL_RenderCopy(renderer, texture, &srcRects[map[cell]], &dstRects[cell]);
+	++cell;
+	goto StartRenderCell;
+EndRenderCell:
 	SDL_RenderPresent(renderer);
+	map[index] = old;
 
 StartEventLoop:
 	if (!SDL_PollEvent(&event)) goto StartDraw;
@@ -46,7 +60,9 @@ StartEventLoop:
 		(SDLK_UP == event.key.keysym.sym) ? (-GRID_COLUMNS) :
 		(SDLK_DOWN == event.key.keysym.sym) ? (GRID_COLUMNS) :
 		(0);
-	index = (index + delta) % (GRID_COLUMNS * GRID_ROWS);
+	nextIndex = (index + delta) % (GRID_COUNT);
+
+	index = (0 == map[nextIndex]) ? (nextIndex) : (index);
 PostKeyDownEvent:
 
 
