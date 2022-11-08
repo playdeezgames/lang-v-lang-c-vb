89ade49ba24528731658c33e94e1896b29a2b305
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-08 04:43:47 -0600

    now with more goto

--- src/DungeonDelverC/Main.c
+++ src/DungeonDelverC/Main.c
@@ -8,46 +8,41 @@ enum {
 };
 int main(int argc, char** argv)
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING))
-	{
-		fprintf(stderr,"%s", SDL_GetError());
-		return -1;
-	}
-	else
-	{
-		SDL_Window* window = 0;
-		SDL_Renderer* renderer = 0;
-		if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer))
-		{
-			//TODO: what about the state of window and renderer?
-			fprintf(stderr, "%s", SDL_GetError());
-			return -1;
-		}
-		else
-		{
-			SDL_RenderSetLogicalSize(renderer, LOGICAL_WIDTH, LOGICAL_HEIGHT);
-			SDL_Event event = {0};
-			int running = 1;
-			do {
-				SDL_RenderClear(renderer);
-				SDL_RenderPresent(renderer);
-				if (SDL_PollEvent(&event))
-				{
-					running = event.type != SDL_QUIT;
-				}
-			} while (running);
-		}
-		if (renderer)
-		{
-			SDL_DestroyRenderer(renderer);
-			renderer = 0;
-		}
-		if (window)
-		{
-			SDL_DestroyWindow(window);
-			window = 0;
-		}
-		SDL_Quit();
-	}
+	SDL_Window* window = 0;
+	SDL_Renderer* renderer = 0;
+	SDL_Event event = { 0 };
+
+	if (SDL_Init(SDL_INIT_EVERYTHING)) goto PreinitializationFailure;
+
+	if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer)) goto PostInitializationFailure;
+
+Draw:
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+HandleEvents:
+	if (!SDL_PollEvent(&event)) goto Draw;
+
+	if (event.type == SDL_QUIT) goto DestroyRenderer;
+
+	goto Draw;
+
+DestroyRenderer:
+	if (!renderer) goto DestroyWindow;
+	SDL_DestroyRenderer(renderer);
+
+DestroyWindow:
+	if (!window) goto CleanUpSDL;
+	SDL_DestroyWindow(window);
+
+CleanUpSDL:
+	SDL_Quit();
 	return 0;
+
+PostInitializationFailure:
+	SDL_Quit();
+
+PreinitializationFailure:
+	fprintf("%s", SDL_GetError());
+	return -1;
 }
