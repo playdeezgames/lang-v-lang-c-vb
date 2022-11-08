58749b34f47949e0e2a26eb25f026b51492ed66e
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-07 19:00:21 -0600

    full size

--- src/DungeonDelverC/Application.c
+++ src/DungeonDelverC/Application.c
@@ -6,10 +6,30 @@ struct Application
 {
 	struct Textures* textures;
 	struct Globals* globals;
+	SDL_Rect dst;
+	SDL_Rect src;
 };
 int Application_HandleCommand(struct Application* ptr, char command)
 {
-	return '.' != command;
+	switch (command)
+	{
+	case '8':
+		ptr->dst.y -= 16;
+		return 1;
+	case '2':
+		ptr->dst.y += 16;
+		return 1;
+	case '4':
+		ptr->dst.x -= 16;
+		return 1;
+	case '6':
+		ptr->dst.x += 16;
+		return 1;
+	case '.':
+		return 0;
+	default:
+		return 1;
+	}
 }
 struct Globals* Application_GetGlobals(struct Application* ptr)
 {
@@ -20,15 +40,24 @@ struct Application* Application_Initialize(struct Globals* globals)
 	struct Application* ptr = Utility_Allocate(sizeof(struct Application));
 	ptr->globals = globals;
 	ptr->textures = Textures_Initialize(globals);
-	SDL_RenderSetLogicalSize(Globals_GetRenderer(ptr->globals), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 	IMG_Init(IMG_INIT_PNG);
+	ptr->src.x = 32;
+	ptr->src.y = 0;
+	ptr->src.w = 16;
+	ptr->src.h = 16;
+	ptr->dst.x = SCREEN_WIDTH/2;
+	ptr->dst.y = SCREEN_HEIGHT/2;
+	ptr->dst.w = 16;
+	ptr->dst.h = 16;
 	return ptr;
 }
 void Application_Draw(struct Application* ptr)
 {
-	SDL_Rect dst = {0,0,16,16};
-	SDL_Rect src = { 32,0,16,16 };
-	SDL_RenderCopy(Globals_GetRenderer(ptr->globals), Textures_get(ptr->textures, TEXTURES_ROMFONT), &src, &dst);
+	SDL_RenderCopy(
+		Globals_GetRenderer(ptr->globals), 
+		Textures_get(ptr->textures, TEXTURES_ROMFONT), 
+		&(ptr->src), 
+		&(ptr->dst));
 }
 void Application_CleanUp(struct Application** ptr)
 {
