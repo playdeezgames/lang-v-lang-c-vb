784e4071f7109b35c492d188842e5f36815db556
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-09 04:40:08 -0600

    do draw

--- src/DungeonDelverC/Main.c
+++ src/DungeonDelverC/Main.c
@@ -16,38 +16,38 @@ int Init(struct Context* context)
 	InitCellMap();
 	return 0;
 }
-int main(int argc, char** argv)
+int HandleEvents(int* index)
 {
-	struct Context context = { 0 };
-	SDL_Event event = { 0 };
-	int index = GRID_COLUMNS/2 + (GRID_ROWS/2) * GRID_COLUMNS;
 	int nextIndex = 0;
 	int delta = 0;
-
-	if (Init(&context)) goto CleanUp;
-
-StartDraw:
-	Draw(&context, index);
-
+	static SDL_Event event = { 0 };
 StartEventLoop:
-	if (!SDL_PollEvent(&event)) goto StartDraw;
+	if (!SDL_PollEvent(&event)) return 1;
 
-	if (event.type == SDL_QUIT) goto EndEventLoop;
-	
+	if (event.type == SDL_QUIT) return 0;
+
 	if (event.type != SDL_KEYDOWN) goto PostKeyDownEvent;
-	delta = 
+	delta =
 		(SDLK_LEFT == event.key.keysym.sym) ? (-1) :
 		(SDLK_RIGHT == event.key.keysym.sym) ? (1) :
 		(SDLK_UP == event.key.keysym.sym) ? (-GRID_COLUMNS) :
 		(SDLK_DOWN == event.key.keysym.sym) ? (GRID_COLUMNS) :
 		(0);
-	nextIndex = (index + delta) % (GRID_COUNT);
-	
-	index = (0 == GetCellMap(nextIndex % GRID_COLUMNS, nextIndex / GRID_COLUMNS)) ? (nextIndex) : (index);
+	nextIndex = (*index + delta) % (GRID_COUNT);
+
+	*index = (0 == GetCellMap(nextIndex % GRID_COLUMNS, nextIndex / GRID_COLUMNS)) ? (nextIndex) : (*index);
 PostKeyDownEvent:
 
 	goto StartEventLoop;
-EndEventLoop:
+}
+int main(int argc, char** argv)
+{
+	struct Context context = { 0 };
+	int index = GRID_COLUMNS/2 + (GRID_ROWS/2) * GRID_COLUMNS;
+
+	if (Init(&context)) goto CleanUp;
+
+	do Draw(&context, index); while (HandleEvents(&index));
 
 CleanUp:
 	CleanUpContext(&context);
