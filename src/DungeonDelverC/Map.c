ec387237de21e12a9e160b225a94f1e1c2504944
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-11-09 13:18:59 -0600

    reformat

--- src/DungeonDelverC/Map.c
+++ src/DungeonDelverC/Map.c
@@ -4,16 +4,18 @@
 static struct Entity* cellMap[GRID_COUNT] = { 0 };
 void InitMap()
 {
-	int index = 0;
 	int column;
 	int row;
-Loop:
-	if (index == GRID_COUNT) return;
-	column = index % GRID_COLUMNS;
-	row = index / GRID_COLUMNS;
-	cellMap[index]= (column == 0 || column == GRID_COLUMNS - 1 || row == 0 || row == GRID_ROWS - 1) ? (GetEntity(ENTITY_WALL)) : (GetEntity(ENTITY_EMPTY));
-	++index;
-	goto Loop;
+	for (int index = 0; index < GRID_COUNT; ++index) 
+		column = index % GRID_COLUMNS, 
+		row = index / GRID_COLUMNS, 
+		cellMap[index] = 
+			(column == 0 || 
+			column == GRID_COLUMNS - 1 || 
+			row == 0 || 
+			row == GRID_ROWS - 1) ? 
+			(GetEntity(ENTITY_WALL)) : 
+			(GetEntity(ENTITY_EMPTY));
 }
 void SetMapEntity(int column, int row, struct Entity* cellType)
 {
